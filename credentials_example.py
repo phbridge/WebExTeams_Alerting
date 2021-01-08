bf4281d0edb48d15c9e56c7eed1a41de83330da0
phbridge <bridges.p.j1@gmail.com>
2021-01-08 21:23:46 +0000

    Initial Commit

---
+++ credentials_example.py
@@ -0,0 +1,8 @@
+MERAKI_API_KEY = "1234567890abcdef1234567890abcdef1234567"
+FLASK_HOST = "::"
+FLASK_PORT = 8050
+FLASK_HOSTNAME = "example.com"
+ABSOLUTE_PATH = "/Users/phbridge/Documents/Git/MerakiMonitor"
+LOGFILE = ABSOLUTE_PATH + "/logs/MerakiMonitor_%s_%s.log" % (FLASK_HOST, FLASK_PORT)
+MERAKI_ORG_ID = "1234567"
+WXT_BOT_NAME = "botname.example"
